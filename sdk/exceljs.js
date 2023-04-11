19b8ca140e283a18b2daaca23f7969102819d13c
Bassea <andre.basse@spiegel.de>
2023-04-11 11:23:24 +0200

    Updated amis to version 2.9.0 and added support for langauge selection

--- sdk/exceljs.js
+++ sdk/exceljs.js
@@ -1,5 +1,5 @@
 ;/*!node_modules/exceljs/dist/exceljs.min.js*/
-amis.define('99cb571', function(require, exports, module, define) {
+amis.define('28b2f6f', function(require, exports, module, define) {
 
   /*! ExcelJS 21-08-2021 */
   
